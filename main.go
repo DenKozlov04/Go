8260429fea8380632e43a515a0b46647561e9d16
Deniss Kozlovs <78017740+DenKozlov04@users.noreply.github.com>
2025-09-16 09:46:23 +0300

    1LD1UZDEVUMS

--- main.go
+++ main.go
@@ -1,10 +1,37 @@
+// Deniss Kozlovs 241RDB330
+// 1 uzdevums
 package main
+
 import "fmt"
 
-func main(){
-	var name string
-	fmt.Print("Write your name:")
-	fmt.Scan(&name)
-	fmt.Println("Your name is",name)
-	
+func main() {
+	// var size int
+	// fmt.Println("Write massiv length:")
+	// _,err := fmt.Scan(&size)
+	// if err != nil || size <2 {
+	// 	fmt.Println("input-output error")
+	// 	return
+	// } else {
+	// 	arr := make([]int, size)
+	// 	fmt.Println("Write numbers:")
+	// 	for i := 0; i < size; i++ {
+	// 		_,err := fmt.Scan(&arr[i])
+	// 		if err != nil || arr[i] < 2 {
+	// 			fmt.Println("input-output error")
+	// 			return
+	// 		}else{
+
+	// 		}
+	// 	}
+	// 	fmt.Println("array", arr)
+	// }
+	var size int
+	fmt.Println("Write your:")
+	_,err := fmt.Scan(&size)
+	if err != nil || size <2 {
+		fmt.Println("input-output error")
+		return
+	} else {
+
+	}
 }
