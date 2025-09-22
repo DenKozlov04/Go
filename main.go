54010d61b89031ca1726a0da28dd7e0427339464
DenKozlov04 <datlovdenis363@gmail.com>
2025-09-22 21:06:43 +0300

    1uzdevums 1LD

--- main.go
+++ main.go
@@ -5,33 +5,21 @@ package main
 import "fmt"
 
 func main() {
-	// var size int
-	// fmt.Println("Write massiv length:")
-	// _,err := fmt.Scan(&size)
-	// if err != nil || size <2 {
-	// 	fmt.Println("input-output error")
-	// 	return
-	// } else {
-	// 	arr := make([]int, size)
-	// 	fmt.Println("Write numbers:")
-	// 	for i := 0; i < size; i++ {
-	// 		_,err := fmt.Scan(&arr[i])
-	// 		if err != nil || arr[i] < 2 {
-	// 			fmt.Println("input-output error")
-	// 			return
-	// 		}else{
-
-	// 		}
-	// 	}
-	// 	fmt.Println("array", arr)
-	// }
-	var size int
-	fmt.Println("Write your:")
-	_,err := fmt.Scan(&size)
-	if err != nil || size <2 {
+	var number int
+	fmt.Println("Write your number:")
+	_,err := fmt.Scan(&number)
+	if err != nil || number <2 {
 		fmt.Println("input-output error")
 		return
 	} else {
-
+		var factor  = 2
+		for factor <= number {
+			if number % factor == 0 {
+				fmt.Print(factor," ")
+				number  = number / factor
+			}else{
+				factor++
+			}
+    	}
 	}
 }
